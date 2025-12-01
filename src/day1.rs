c5c2f2cdb02806c4ecc25f2632bc30cdab7273c3
Mårten Blankfors <marten@blankfors.se>
2025-12-01 08:56:44 +0100

    feat: Day 1 Part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,5 @@
 pub fn p1(input: &str) -> String {
-    let (dial, num_zeros) = input
+    let (_dial, num_zeros) = input
         .trim()
         .lines()
         .fold((50, 0), |(dial, mut num_zeros), line| {
@@ -22,11 +22,30 @@ pub fn p1(input: &str) -> String {
 }
 
 pub fn p2(input: &str) -> String {
-    todo!();
-}
+    let (_dial, num_zeros) = input
+        .trim()
+        .lines()
+        .fold((50, 0), |(dial, mut num_zeros), line| {
+            let val: i32 = line[1..].trim().parse().unwrap();
+            let (dial, zeros): (i32, i32) = match line.chars().next().unwrap() {
+                'L' => (
+                    dial - val,
+                    (val - dial).div_euclid(100) + 1 - if dial == 0 { 1 } else { 0 },
+                ),
+                'R' => (dial + val, (dial + val).div_euclid(100)),
+                _ => panic!("nope"),
+            };
 
-struct Dial {
-    value: i64,
+            println!("{line}: Dial {dial} npz {zeros}");
+
+            num_zeros += zeros;
+
+            let dial = dial.rem_euclid(100);
+
+            (dial, num_zeros)
+        });
+
+    num_zeros.to_string()
 }
 
 use crate::solution::Solution;
