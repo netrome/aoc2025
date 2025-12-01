1d87c264e28ab486cd0321957dc802de939b3a8d
Mårten Blankfors <marten@blankfors.se>
2025-12-01 08:16:08 +0100

    feat: Day 1 part 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,10 +1,32 @@
 pub fn p1(input: &str) -> String {
-    println!("{input}");
-    todo!();
+    let (dial, num_zeros) = input
+        .trim()
+        .lines()
+        .fold((50, 0), |(dial, mut num_zeros), line| {
+            let val: i32 = line[1..].trim().parse().unwrap();
+            let dial: i32 = match line.chars().next().unwrap() {
+                'L' => dial - val,
+                'R' => dial + val,
+                _ => panic!("nope"),
+            }
+            .rem_euclid(100);
+
+            if dial == 0 {
+                num_zeros += 1;
+            }
+
+            (dial, num_zeros)
+        });
+
+    num_zeros.to_string()
 }
 
 pub fn p2(input: &str) -> String {
     todo!();
+}
+
+struct Dial {
+    value: i64,
 }
 
 use crate::solution::Solution;
