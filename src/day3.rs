1da4aea473d999555873cc6f7462ad6ab6838229
Mårten Blankfors <marten@blankfors.se>
2025-12-03 09:34:40 +0100

    feat: Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,39 +1,47 @@
 pub fn p1(input: &str) -> String {
-    let banks: Vec<Vec<u32>> = input
+    let banks: Vec<Vec<u64>> = input
         .trim()
         .lines()
-        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as u64)
+                .collect()
+        })
         .collect();
 
     banks
         .iter()
         .map(|bank| largest_possible_joltage(&bank))
-        .sum::<u32>()
+        .sum::<u64>()
         .to_string()
 }
 
 pub fn p2(input: &str) -> String {
-    let banks: Vec<Vec<u32>> = input
+    let banks: Vec<Vec<u64>> = input
         .trim()
         .lines()
-        .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as u64)
+                .collect()
+        })
         .collect();
 
     banks
         .iter()
         .map(|bank| largest_possible_joltage_p2(&bank, 12))
-        .sum::<u32>()
+        .sum::<u64>()
         .to_string()
 }
 
-fn largest_possible_joltage(bank: &[u32]) -> u32 {
+fn largest_possible_joltage(bank: &[u64]) -> u64 {
     let (largest_idx, largest) = find_largest(&bank[0..bank.len() - 1]);
     let (_, second_largest_in_remainder) = find_largest(&bank[largest_idx + 1..]);
 
     largest * 10 + second_largest_in_remainder
 }
 
-fn largest_possible_joltage_p2(bank: &[u32], n: usize) -> u32 {
+fn largest_possible_joltage_p2(bank: &[u64], n: usize) -> u64 {
     let mut start_idx = 0;
     let mut joltage = 0;
 
@@ -46,7 +54,7 @@ fn largest_possible_joltage_p2(bank: &[u32], n: usize) -> u32 {
     joltage
 }
 
-fn find_largest(bank: &[u32]) -> (usize, u32) {
+fn find_largest(bank: &[u64]) -> (usize, u64) {
     let max = bank.iter().max().unwrap();
     let max_idx = bank.iter().find_position(|val| *val == max).unwrap().0;
 
